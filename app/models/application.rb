1b4f9a1fc98fdc3555d5e33da658de5fe301ce3d
Joey Haas <josephthaas@gmail.com>
2021-04-05 16:41:21 -0600

    created new tables and associations with tests

---
+++ app/models/application.rb
@@ -0,0 +1,10 @@
+class Application < ApplicationRecord
+  validates :name_of_applicant, presence: true
+  validates :street_address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip_code, presence: true
+  validates :description, presence: true
+  has_many :application_pets
+  has_many :pets, through: :application_pets
+end
