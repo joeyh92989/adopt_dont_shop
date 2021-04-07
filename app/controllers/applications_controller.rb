3d88f2eb4d8a198e1dc7979c35e12edd68aeda85
Joey Haas <josephthaas@gmail.com>
2021-04-06 21:56:07 -0600

    can submit an application

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -7,23 +7,31 @@ class ApplicationsController < ApplicationController
       @application = Application.find(params[:id])
       @pet_search = Pet.search(params[:search])
       @pets = @application.pets
+      
     else
     @application = Application.find(params[:id])
     @pets = @application.pets
     end
   end
   def new
-    
   end
+
   def create
     application = Application.create!(application_params)
     application.save
     redirect_to "/applications/#{application.id}"
   end
 
+  def update
+    binding.pry
+    application = Application.find(params[:id])
+    application.update!(application_params)
+    redirect_to "/applications/#{application.id}"
+  end
+
   private
 
   def application_params
-    params.permit(:name_of_applicant, :street_address, :city, :state, :zip_code, :description)
+    params.permit(:name_of_applicant, :street_address, :city, :state, :zip_code, :description, :status)
   end
 end
